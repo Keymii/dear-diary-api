731221eddc9c8f5c70938e233c049c14bd801950
Tarus <tarus@Tarus-Pandes-MacBook-Air.local>
2023-02-21 22:06:43 +0530

    home placeholder made

---
+++ dear_diary_api/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('home/<int:id>', views.home, name='home'),
+]
---
+++ dear_diary_api/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import MasterTable
+
+@api_view(['GET'])
+def home(request, id):
+    table=MasterTable.objects.all()
+    user=table.filter(userdataid=id)
+    data={}
+    for sec in user.section.all():
+        s=user.filter(section=sec)
+        for page in s.page.all():
+            data={**data, s.section: page}
+    return Response(data)
