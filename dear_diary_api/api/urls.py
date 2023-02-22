8974f02ca2b484ffeb1c1e9daa2ff0f7459d8537
mahesh_kumar <mahesh.99prajapat@gmail.com>
2023-02-22 16:52:11 +0530

    adding name field and homepage and userExistence check view and api_view

--- dear_diary_api/api/urls.py
+++ dear_diary_api/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+     path('',views.home),
+    path('api/',views.api),
     path('register/',views.addUser),
-    path('',views.home)
+    path("<str:id>",views.userExist),
 ]
--- dear_diary_api/main/models.py
+++ dear_diary_api/main/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class userLogin(models.Model):
+    name=models.CharField(max_length=200)
+
     userid=models.CharField(max_length=14,primary_key=True)
     pswd=models.CharField(max_length=30)
-    
+
+    def __str__(self):
+        return self.userid
