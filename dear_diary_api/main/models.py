5579b4464e9948d43900924f5399fc91117c8a58
Tarus <tarus@Tarus-Pandes-MacBook-Air.local>
2023-03-03 21:35:23 +0530

    userid changed to text field

--- dear_diary_api/main/models.py
+++ dear_diary_api/main/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class userLogin(models.Model):
     name=models.CharField(max_length=200)
-    userid=models.CharField(max_length=14,primary_key=True)
+    userid=models.TextField(primary_key=True)
     pswd=models.CharField(max_length=30)
     def __str__(self):
         return self.userid
